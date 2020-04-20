dd0744d4dec6bd15c17813a447848cdfe35abb0b
AdityaaMK <44482076+AdityaaMK@users.noreply.github.com>
2020-04-20 09:42:48 -0400

    Fragment Demo

---
+++ Mobile Application Development 2019-20/FragmentDemo/app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.fragmentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button replaceButton;
+    FragmentTransaction fragmentTransaction;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        replaceButton = findViewById(R.id.button);
+
+        fragmentManager = getSupportFragmentManager();
+
+        //Begin first transaction
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        //Create bottom fragment and add to layout on bottom of XML
+        BottonFragment bottonFragment = new BottonFragment();
+        fragmentTransaction.add(R.id.id_bottom,bottonFragment);
+
+        //commit the transaction (end)
+        fragmentTransaction.commit();
+
+
+    }
+}
